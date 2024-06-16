c95f410a1aae763466f72c6651a4e9b2fa7b0f54
nilaytayade <136469073+nilaytayade@users.noreply.github.com>
2024-06-16 12:04:50 +0530

    Add files via upload

---
+++ theme.config.jsx
@@ -0,0 +1,34 @@
+export default {
+  logo: (
+    <>
+      <img
+        width="50"
+        height="50"
+        viewBox="0 0 24 24"
+        src="https://d10gf95rxwihx0.cloudfront.net/20240615_140019.png"
+      ></img>
+      <span style={{ marginLeft: ".4em", fontWeight: 800 }}>OASIS</span>
+    </>
+  ),
+  project: {
+    link: "https://github.com/nilaytayade/oasis-docs",
+  },
+docsRepositoryBase:"https://github.com/nilaytayade/oasis-docs",
+  banner: {
+    key: "2.0-release",
+    text: <a>🎉 OASIS 2.0 is LIVE🚀</a>,
+    },
+  
+    
+  footer: {
+    text: (
+      <span>
+        OASIS {new Date().getFullYear()} ©{" "}
+        <a href="" target="_blank">
+          Oasis🌴
+        </a>
+        .
+      </span>
+    ),
+  },
+};
