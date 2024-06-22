b25b98f8ae96596a242ed3ce39faabc9aa53d2bf
nilaytayade <136469073+nilaytayade@users.noreply.github.com>
2024-06-22 13:50:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withNextra = require("nextra")({
-  theme: "nextra-theme-blog",
-  themeConfig: "./theme.config.js",
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
 });
-module.exports = withNextra({
-  basePath: "/pages/intro",
-});
+
+module.exports = withNextra();
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
